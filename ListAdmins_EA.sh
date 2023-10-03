f19d1fced230d508947b930b0d07f80f7269ef9f
Ian Myers <117406658+iangmyers@users.noreply.github.com>
2023-10-03 11:37:09 -0400

    Update ListAdmins_EA.sh

--- ListAdmins_EA.sh
+++ ListAdmins_EA.sh
@@ -2,12 +2,17 @@
 
 ###########
 # Created by Ian Myers
-# 3/21/23 
+# 3/21/23 updated 10/3/23
 # Finds and list all admin accounts on a device. This can be used in conjusttion with a Smart Group and policy to clean up admin accounts
 ###########
 
-# get a list of all users who are members of the "admin" group
-adminUsers=$(dscl . -read /Groups/admin GroupMembership | cut -c18- | tr ' ' '\n')
+#!/bin/bash
 
-# output the list of admin users as a comma-separated string
-echo "<result>$adminUsers</result>"
+# Get a list of admin users (excluding root)
+admin_users=$(dscl . -read /Groups/admin GroupMembership | tr " " "\n" | grep -v -e '^root$')
+
+# Convert the list to a comma-separated string without the prefix
+admin_user_list=$(echo "$admin_users" | paste -s -d, - | sed 's/GroupMembership:,//')
+
+# Output the admin user list
+echo "<result>$admin_user_list</result>"
